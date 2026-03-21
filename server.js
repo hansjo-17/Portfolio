0b8bc62194f943839241dd661dcda281adbce4a5
hansjo-17 <hansjothomas17@gmail.com>
2026-03-21 15:37:38 +0530

    5

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv").config;
 //dotenv.config({ path: __dirname + "/.env" });//
 const db = require("./db");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
