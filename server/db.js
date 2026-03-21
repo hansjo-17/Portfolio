8656ffd93149b32b38e0fd3e235e7d4209d8d141
hansjo-17 <hansjothomas17@gmail.com>
2026-03-21 14:19:20 +0530

    1

--- server/db.js
+++ server/db.js
@@ -1,10 +1,21 @@
 const mysql = require("mysql2");
-const db = mysql.createConnection(process.env.DATABASE_URL);
+
+const url = new URL(process.env.DATABASE_URL);
+
+const db = mysql.createConnection({
+  host: url.hostname,
+  user: url.username,
+  password: url.password,
+  database: url.pathname.replace("/", ""),
+  port: url.port
+});
+
 db.connect((err) => {
   if (err) {
-    console.error("DB connection failed:", err);
+    console.error("Database connection failed:", err);
   } else {
     console.log("Connected to MySQL database");
   }
 });
+
 module.exports = db;
